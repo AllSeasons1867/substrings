85bb532e7ed427cc070268d6bb976f7075dfb216
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-12 21:55:42 -0600

    solution with simple tests completed

--- substring.rb
+++ substring.rb
@@ -1,7 +1,16 @@
 def substring(string, dictionary)
     histogram = Hash.new(0)
 
-    
+    dictionary.each do |word|
+        if string.include?(word)
+            histogram[word] += 1
+        end
+    end
+
+    return histogram
 end
 
-substring("portfolio",["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
+puts substring("below the horn down below i own",["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
+
+puts substring("the all terrain portfolio",["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","terrain", "portfolio"])
+
