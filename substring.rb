82b3fd19581fb36936e9334b273928b28085cfba
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-12 21:46:42 -0600

    setup basic method structure

---
+++ substring.rb
@@ -0,0 +1,7 @@
+def substring(string, dictionary)
+    histogram = Hash.new(0)
+
+    
+end
+
+substring("portfolio",["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
